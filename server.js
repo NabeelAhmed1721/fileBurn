6842efedf5d6fe85b1c792b00b6fce7159c5e060
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2019-10-05 00:48:21 -0400

    force https

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Force a https connection
+//Made for Heroku
+app.use((req, res, next) => {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+});
+
+//Server Initializes
 app.set('view engine', 'pug'); //view engine set
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+//Index Router
 app.get('/',(req,res)=>{
     res.render('index', {title: 'fileburn.', message: 'Welcome!'});
 });
 
-
+//404 Router
 app.use((req, res, next) => {
     return res.status(404).render('404', { message: req.url+' was not found :(' });
 });
 
-
+//Listen
 app.listen(PORT, ()=>{
     console.log(`Server up on port: ${PORT}`);
 });
