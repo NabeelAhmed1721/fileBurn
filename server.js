cc08039710249fac24e67c8e047c3f32836c114d
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2019-10-05 00:41:38 -0400

    pug init and basic 404 page router

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'pug'); //view engine set
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/',(req,res)=>{
-    res.send("fileburn.");
+    res.render('index', {title: 'fileburn.', message: 'Welcome!'});
 });
+
+
+app.use((req, res, next) => {
+    return res.status(404).render('404', { message: req.url+' was not found :(' });
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`Server up on port: ${PORT}`);
